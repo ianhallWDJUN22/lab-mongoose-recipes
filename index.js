f4173725e764713cbccfb235b1aa77e86aff03ff
Ian Hall <ianmhall6@gmail.com>
2022-06-30 14:23:42 -0400

    lab complete

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  // .then(x => {
+  //   console.log(`Connected to the database: "${x.connection.name}"`);
+  //   // Before adding any recipes to the database, let's remove all existing ones
+  //   return Recipe.deleteMany()
+  // })
+  // .then(() => {
+  //   Recipe.insertMany(data)
+  //   .then(recipe => console.log(recipe.title))
+  //   .catch(error => console.log('Error is:', error))
+
+  // })
+  // .catch(error => {
+  //   console.error('Error connecting to the database', error);
+  
+  //  Recipe.findOneAndUpdate(
+    //    {title: "Rigatoni alla Genovese"},
+    //    {duration: 100}
+    //  )
+    //  .then(success => console.log('Update successfull', success))
+    //  .catch(err => console.log('UH OH:', err))
+    
+    Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(success => console.log('One Deleted', success ))
+    .catch(err => console.log('UH OH:', err))
+    // });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String},
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef' ]},
+  ingredients: {type: [String]},
+  cuisine: {type: String},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number},
+  creator: {type: String},
+  created: {type: Date}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
